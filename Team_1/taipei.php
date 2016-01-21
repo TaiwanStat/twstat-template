a0da6c09017f346f40abdfaa967910f5e4f8c063
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-01-22 00:03:45 +0800

    add support of taipei metro data

--- Team_1/taipei.php
+++ Team_1/taipei.php
@@ -3,10 +3,33 @@ header('content-type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');//allow cross site ajax
 header('Access-Control-Allow-Methods: GET');
 
-$data=$_GET['type'];
-$url="http://data.taipei/".$data."/".$_GET["data"];
-if($_GET["data"]=="Stop"){
+$type=$_GET["type"];
+$data=$_GET["data"];
+$url="http://data.taipei/".$type."/".$data;
+if($data=="Stop" && $type=="bus" ){
+    //Bus Stop Cache
     $url="GetSTOP.json";
+}
+
+//for Taipei Metro data
+if($type=="mrt"){
+    switch ($data) {
+        //捷運車站出入口
+        case 'entrance':
+            $rid="69bce456-b934-42fd-be18-2d3cb1c3ef84";
+            break;
+
+        //轉乘車站轉乘步行時間
+        case 'walk':
+            $rid="cb7c5713-44f6-43e2-b736-f407af94afef";
+            break;
+
+        //捷運列車到站資料
+        case 'arrive':
+            $rid="55ec6d6e-dc5c-4268-a725-d04cc262172b";
+            break;
+    }
+    $url="http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=".$rid;
 }
 
 $data  = file_get_contents("compress.zlib://".$url);
