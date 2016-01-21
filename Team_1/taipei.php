5e7737260a624ff11693e0d079422fcca9560829
Yu Hsun Lee <andylee0914.tw@gmail.com>
2016-01-21 22:58:18 +0800

    add support of other data

--- Team_1/taipei.php
+++ Team_1/taipei.php
@@ -3,7 +3,8 @@ header('content-type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');//allow cross site ajax
 header('Access-Control-Allow-Methods: GET');
 
-$url="http://data.taipei/bus/".$_GET["data"];
+$data=$_GET['type'];
+$url="http://data.taipei/".$data."/".$_GET["data"];
 if($_GET["data"]=="Stop"){
     $url="GetSTOP.json";
 }
