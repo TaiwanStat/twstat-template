79101e3e52371c1adc2967e18ebef8dda32c0f07
yakiyang <ninayang1104@yahoo.com.tw>
2017-01-05 21:08:47 +0800

    update

--- Team_2/test.py
+++
@@ -1,4 +0,0 @@
-from firebase import Firebase
-f = Firebase('https://radiant-fire-7063.firebaseio.com/test')
-r = f.push({'date':'2015-11-09','WTI':43.870,'Dubai':44.370,'Brent':47.830,'type':1})
-##{'date':'2015-11-09','WTI':'43.870','Dubai':'44.370','Brent':'47.830','type':'1'}
